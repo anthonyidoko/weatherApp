d51d085634a72847cd0edb19581cb0fd6a20f608
anthonyidoko <idokoanthony86@yahoo.com>
2022-03-21 11:17:58 +0100

    Added dependencies for navigation, nav_graph, NavHostFragment and NavController

--- app/src/main/java/com/example/weatherapp/ui/DetailFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/DetailFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
     private lateinit var _binding: FragmentDetailBinding
-    private val binding: FragmentDetailBinding = _binding
+    private val binding: FragmentDetailBinding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentDetailBinding.bind(view)
         binding.tvOneTwo.text = "Detail Fragment"
+        binding.tvOneTwo.setOnClickListener {
+            val action = DetailFragmentDirections.actionDetailFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
+
     }
 
 
