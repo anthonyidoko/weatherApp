42353da59457b0cf65b3dbc1f7c5b0b655905188
anthonyidoko <idokoanthony86@yahoo.com>
2022-03-28 11:30:16 +0100

    created the two app fragments

--- app/src/main/java/com/example/weatherapp/ui/DetailFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/DetailFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.navArgs
 import com.example.weatherapp.R
+import com.example.weatherapp.data.model.weatherDet.WeatherDetailResponseData
 import com.example.weatherapp.databinding.FragmentDetailBinding
+import com.example.weatherapp.viewModel.WeatherDetailViewModel
+import com.example.weatherapp.util.*
+import com.squareup.picasso.Picasso
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
     private lateinit var _binding: FragmentDetailBinding
     private val binding: FragmentDetailBinding get() = _binding
+    private val args: DetailFragmentArgs by navArgs()
+    private val detailViewModel: WeatherDetailViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,12 +34,82 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentDetailBinding.bind(view)
-        binding.tvOneTwo.text = "Detail Fragment"
-        binding.tvOneTwo.setOnClickListener {
-            val action = DetailFragmentDirections.actionDetailFragmentToHomeFragment()
-            findNavController().navigate(action)
-        }
 
+        observeWeatherDetail(args.id)
+
+    }
+
+    private fun observeWeatherDetail(id: Int) {
+        detailViewModel.getWeatherDetailFromDb(id).observe(viewLifecycleOwner, Observer {
+            setWeatherDetailDataOnViews(it)
+        })
+
+    }
+
+    private fun setWeatherDetailDataOnViews(data: WeatherDetailResponseData?) {
+        when{
+            data != null -> {
+                binding.tvDetailNotReady.visibility = View.GONE
+                binding.tvCity.text = data.city?.name?.concatenate(data.city.country)
+                binding.tvDay.text = data.list?.get(0)?.let{ it.dt }?.formatDate()
+                binding.tvFeelsLikeValue.text = data.list?.get(0)?.main?.feels_like?.convertTemperatureFromKelvinToCelcius(
+                    false) ?: ""
+                binding.tvWindValue.text = String.format("${data.list?.get(0)?.wind?.speed}Km/h")
+
+                binding.tvHumidityValue.text = data.list?.get(0)?.main?.humidity?.setHumidity()
+
+                binding.tvTypePressure.text = String.format("${data.list?.get(0)?.main?.pressure}ln")
+
+                binding.tvVisibility.text = String.format("${data.list?.get(0)?.visibility}")
+
+                binding.tvDewPointValue.text = ("${data.list?.get(0)?.clouds?.all}")
+
+
+                binding.tvTemperatureValue.text =
+                    data.list?.get(0)?.main?.temp?.convertTemperatureFromKelvinToCelcius(true) ?:""
+
+                binding.tvDayOne.text = data.list?.get(0)?.dt?.extractWeekDay()
+                binding.tvDayTwo.text = data.list?.get(9)?.dt?.extractWeekDay()
+                binding.tvDayThree.text = data.list?.get(18)?.dt?.extractWeekDay()
+                binding.tvDayFour.text = data.list?.get(26)?.dt?.extractWeekDay()
+                binding.tvDayFive.text = data.list?.get(33)?.dt?.extractWeekDay()
+                binding.tvHumidityOne.text = data.list?.get(0)?.main?.humidity?.setHumidity()
+                binding.tvHumidityTwo.text = data.list?.get(9)?.main?.humidity?.setHumidity()
+                binding.tvHumidityThree.text =data.list?.get(18)?.main?.humidity?.setHumidity()
+                binding.tvHumidityFour.text = data.list?.get(26)?.main?.humidity?.setHumidity()
+                binding.tvHumidityFive.text = data.list?.get(33)?.main?.humidity?.setHumidity()
+
+                binding.tvWeatherDecs.text = data.list?.get(0)?.weather?.get(0)?.description ?: ""
+
+                Picasso.get()
+                    .load("${IMAGE_BASE_URL}${data.list?.get(0)?.weather?.get(0)?.icon}${IMAGE_END_URL}")
+                    .into(binding.ivWeatherIcon)
+
+                Picasso.get()
+                    .load("${IMAGE_BASE_URL}${data.list?.get(0)?.weather?.get(0)?.icon}${IMAGE_END_URL}")
+                    .into(binding.ivDayOne)
+
+                Picasso.get()
+                    .load("${IMAGE_BASE_URL}${data.list?.get(9)?.weather?.get(0)?.icon}${IMAGE_END_URL}")
+                    .into(binding.ivDayTwo)
+
+                Picasso.get()
+                    .load("${IMAGE_BASE_URL}${data.list?.get(18)?.weather?.get(0)?.icon}${IMAGE_END_URL}")
+                    .into(binding.ivDayThree)
+
+                Picasso.get()
+                    .load("${IMAGE_BASE_URL}${data.list?.get(26)?.weather?.get(0)?.icon}${IMAGE_END_URL}")
+                    .into(binding.ivDayFour)
+
+                Picasso.get()
+                    .load("${IMAGE_BASE_URL}${data.list?.get(33)?.weather?.get(0)?.icon}${IMAGE_END_URL}")
+                    .into(binding.ivDayFive)
+
+            }
+            else -> {
+                binding.tvDetailNotReady.visibility = View.VISIBLE
+            }
+        }
     }
 
 
---
+++ app/src/main/java/com/example/weatherapp/ui/WeatherFragment.kt
@@ -0,0 +1,193 @@
+package com.example.weatherapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherapp.R
+import com.example.weatherapp.data.model.weather.ServiceData
+import com.example.weatherapp.data.model.weather.WeatherDataResponse
+import com.example.weatherapp.databinding.FragmentWeatherBinding
+import com.example.weatherapp.viewModel.MainViewModel
+import com.example.weatherapp.util.*
+import com.example.weatherapp.util.adapter.WeatherAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WeatherFragment : Fragment(), SearchView.OnQueryTextListener {
+
+    private lateinit var _binding: FragmentWeatherBinding
+    private val binding: FragmentWeatherBinding get() = _binding
+    private val mainViewModel: MainViewModel by viewModels()
+    private val weatherAdapter by lazy { WeatherAdapter() }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_weather, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentWeatherBinding.bind(view)
+
+        checkInternetConnection()
+        observeWeatherFromDb()
+        mainViewModel.fetchWeatherFromRoom()
+        setUpRecyclerView()
+        getFavouriteWeather()
+//        mainViewModel.fetchWeatherFromApiAndSaveToRoomDatabase(cities)
+        mainViewModel.fetchWeatherFromApiAndSaveToRoomDatabase()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_item, menu)
+        val search = menu.findItem(R.id.menu_search)
+        val searchView = search.actionView as SearchView
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(this)
+    }
+
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        search(query)
+        return true
+    }
+
+    override fun onQueryTextChange(query: String?): Boolean {
+        search(query)
+        return true
+    }
+
+    private fun search(query: String?) {
+        if (query != null) {
+            searchDatabase(query)
+        }
+    }
+
+    private fun searchDatabase(query: String) {
+        val searchQuery = "%$query%"
+        searchDbForData(searchQuery)
+    }
+
+    private fun observeWeatherFromDb() {
+        mainViewModel.fetchWeatherFromRoom().observe(viewLifecycleOwner, Observer {
+            mainViewModel.numberOfWeatherDataInDatabase = it.size
+            weatherAdapter.differ.submitList(it)
+            when {
+                it.size == cities.size -> {
+                    showViews(
+                        binding.tvPlsWait1,
+                        binding.tvPlsWait,
+                        binding.weatherPgBar,
+                        binding.weatherList,
+                        SHOW,
+                        SHOW
+                    )
+                }
+                it.size > 1 && it.size < cities.size -> {
+                    showViews(
+                        binding.tvPlsWait1,
+                        binding.tvPlsWait,
+                        binding.weatherPgBar,
+                        binding.weatherList,
+                        SHOW,
+                        DONT_SHOW
+                    )
+                }
+                else -> {
+                    showViews(
+                        binding.tvPlsWait1,
+                        binding.tvPlsWait,
+                        binding.weatherPgBar,
+                        binding.weatherList,
+                        DONT_SHOW,
+                        DONT_SHOW
+                    )
+
+                }
+            }
+
+        })
+    }
+
+    private fun searchDbForData(searchString: String) {
+        mainViewModel.searchedDatabaseForWeather(searchString)
+            .observe(viewLifecycleOwner, Observer {
+                weatherAdapter.differ.submitList(it)
+            })
+    }
+
+    private fun setUpRecyclerView() {
+        val recyclerView = binding.weatherList
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.adapter = weatherAdapter
+
+        weatherAdapter.setOnItemClickListener(object : WeatherAdapter.OnItemClickListener {
+            override fun onItemClick(item: WeatherDataResponse) {
+                val action =
+                    WeatherFragmentDirections.actionHomeFragmentToDetailFragment(item.id)
+                findNavController().navigate(action)
+            }
+
+        })
+
+        weatherAdapter.setClickListenerForFavouriteWeather(object :
+            WeatherAdapter.OnItemClickListener {
+            override fun onItemClick(item: WeatherDataResponse) {
+                mainViewModel.updateWeather(item.copy(isFavourite = !item.isFavourite))
+            }
+        })
+    }
+
+    private fun getFavouriteWeather() {
+        mainViewModel.getFavouriteWeather().observe(viewLifecycleOwner, Observer {
+            val serviceIntent = Intent(activity, WeatherService::class.java)
+            if (it.isNotEmpty()) {
+
+                serviceIntent.putExtra(
+                    INTENT_DATA,
+                    ServiceData(it[0].main.temp, it[0].main.feels_like, it[0].weather[0].description,it[0].name)
+                )
+            } else {
+                serviceIntent.putExtra(INTENT_DATA, ServiceData(NO_TEMP, NO_TEMP, NO_CITY))
+            }
+            activity?.startService(serviceIntent)
+        })
+    }
+
+    private fun checkInternetConnection() {
+        mainViewModel.networkState.observe(viewLifecycleOwner, Observer {
+            binding.tvPlsWait.text = it
+            if (it.equals(NO_INTERNET)) {
+                showViews(
+                    binding.tvPlsWait1,
+                    binding.tvPlsWait,
+                    binding.weatherPgBar,
+                    binding.weatherList,
+                    SHOW,
+                    SHOW
+                )
+                binding.tvPlsWait.visibility = View.VISIBLE
+            }
+
+        })
+    }
+
+
+
+
+}
